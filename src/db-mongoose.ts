942913c7c948b2d56a4878e9b63e5bd6c343744f
wmkdev <wfmellado@gmail.com>
2020-05-23 20:10:38 -0300

    close #28

--- src/db-mongoose.ts
+++ src/db-mongoose.ts
@@ -19,6 +19,7 @@ export default async function connect(
     const defaultOptions: mongoose.ConnectionOptions = {
       useCreateIndex: true,
       useNewUrlParser: true,
+      useUnifiedTopology: true,
       ...options.connectionOptions,
     };
     return await mongoose.connect(options.uri, defaultOptions).then(
